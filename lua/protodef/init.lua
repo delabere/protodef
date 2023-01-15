56a4919603ed48a49a02cbbedfd0c56fdc2c6728
Jack Rickards <jackrickards@monzo.com>
2023-01-15 20:24:16 +0000

    make most functions private and update tests

--- lua/protodef/init.lua
+++ lua/protodef/init.lua
@@ -13,10 +13,10 @@ M.protodef = function()
     local filename, line_number, col
     if string.match(current_file, ".*proto$") ~= nil then
         -- if we are in a proto file, find the handler function
-        filename, line_number = M.parse_proto(current_file, current_word)
+        filename, line_number = M._parse_proto(current_file, current_word)
     elseif string.match(current_file, ".*go$") ~= nil then
         -- we we are in the go file, find the proto message definition
-        filename, line_number, col = M.parse_go(current_word)
+        filename, line_number, col = M._parse_go(current_word)
     else
         print("operation not supported for current filetype")
         return
@@ -37,17 +37,17 @@ M.protodef = function()
     vim.cmd(":call cursor(" .. line_number .. "," .. col .. ")")
 end
 
-M.parse_go = function(word)
+M._parse_go = function(word)
     -- the text in the buffer, used later to grab the import line
     local buffer_text_1 = vim.api.nvim_buf_get_lines(0, 0, -1, false)
     local buffer_text = table.concat(buffer_text_1, "\n")
-    local import_alias = M.import_alias(word)
+    local import_alias = M._import_alias(word)
 
 
     local import_alias_regex = import_alias .. ' .-wearedev/(service.-)["\\]'
     local import_line = string.match(vim.inspect(buffer_text), import_alias_regex)
 
-    local message = M.message_name(word)
+    local message = M._message_name(word)
 
     local proto_path = vim.fn.resolve(vim.fn.getcwd() .. "/" .. import_line)
     --print("proto path", proto_path)
@@ -58,35 +58,36 @@ M.parse_go = function(word)
     local rg_last_line = result[#result]
 
     if rg_last_line == nil then print("not an existing proto message type") return end
-    local filename, line_number = M.rg_parse(rg_last_line)
+    local filename, line_number = M._rg_parse(rg_last_line)
     return filename, line_number, 9
 end
 
-M.parse_proto = function(file, word)
+M._parse_proto = function(file, word)
     local service = string.match(file, "(.-)/")
     local search_path = vim.fn.resolve(vim.fn.getcwd() .. "/" .. service)
     local rg_search = "rg 'func.*ctx.*" .. word .. "' '" .. search_path .. "' -g '*.go' -n --column"
 
     local result = vim.fn.systemlist(rg_search)
     local rg_last_line = result[#result]
+
     if rg_last_line == nil then print("not an existing proto message type") return end
-    local filename, line_number = M.rg_parse(rg_last_line)
+    local filename, line_number = M._rg_parse(rg_last_line)
     return filename, line_number
 end
 
-M.import_alias = function(cWord)
+M._import_alias = function(cWord)
     local clean_cWord = string.gsub(cWord, "[*&\\){}\\(\\),]", "")
     local import_alias = string.match(clean_cWord, "(.*)%.")
     return import_alias
 end
 
-M.message_name = function(cWord)
+M._message_name = function(cWord)
     local clean_cWord = string.gsub(cWord, "[*&\\){},]", "")
     local import_alias = string.match(clean_cWord, "%.(.*)")
     return import_alias
 end
 
-M.message_clean = function(cWord)
+M._message_clean = function(cWord)
     print("cword", cWord)
     local word = string.match(cWord, "message (.*)")
     return word
@@ -94,7 +95,7 @@ end
 
 -- rg_parse gets the filename and line_number from the result
 -- of a ripgrep command where the -n flag has been used
-M.rg_parse = function(rip_grep_line)
+M._rg_parse = function(rip_grep_line)
     local filename = string.match(rip_grep_line, "(.+):%d+:%d+")
     local line_number = string.match(rip_grep_line, ":(%d+):")
     return filename, line_number
--- tests/protodef_spec.lua
+++ tests/protodef_spec.lua
@@ -6,33 +6,31 @@ describe("protodef", function()
 
     it("test ripgrep parsing ripgrep output", function()
         local test_rg = "proto/potsavingsprovider.proto:590:9:message GETRateHistoryResponse{"
-        local fn, ln, cn = require("protodef").rg_parse(test_rg)
+        local fn, ln = require("protodef")._rg_parse(test_rg)
         assert.equals('proto/potsavingsprovider.proto', fn)
         assert.equals('590', ln)
-        assert.equals(9, cn)
     end)
 
     it("test ripgrep parsing ripgrep output", function()
         local test_rg = "/Users/jackrickards/src/github.com/monzo/wearedev/service.pot/proto/pot.proto:301:1:message GETBalanceRequest {"
-        local fn, ln, cn = require("protodef").rg_parse(test_rg)
+        local fn, ln = require("protodef")._rg_parse(test_rg)
         assert.equals('/Users/jackrickards/src/github.com/monzo/wearedev/service.pot/proto/pot.proto', fn)
         assert.equals('301', ln)
-        assert.equals(9, cn)
     end)
 
     it("get the import alias from the cWORD when in go-file", function()
         local cWord = "*potproto.GETBalanceRequest)"
-        assert.equals('potproto', require("protodef").import_alias(cWord))
+        assert.equals('potproto', require("protodef")._import_alias(cWord))
     end)
 
     it("get the import alias from the cWORD when in go-file", function()
         local cWord = "(*potproto.GETBalanceRequest,"
-        assert.equals('potproto', require("protodef").import_alias(cWord))
+        assert.equals('potproto', require("protodef")._import_alias(cWord))
     end)
 
     it("get the import alias from the cWORD when in a proto", function()
         local cWord = "PUTUpdateRequest"
-        assert.equals(nil, require("protodef").import_alias(cWord))
+        assert.equals(nil, require("protodef")._import_alias(cWord))
     end)
     it("get the import line from the import alias", function()
     end)
@@ -42,6 +40,6 @@ describe("protodef", function()
 
     it("get the message name from the cWORD", function()
         local cWord = "*potproto.GETBalanceRequest)"
-        assert.equals('GETBalanceRequest', require("protodef").message_name(cWord))
+        assert.equals('GETBalanceRequest', require("protodef")._message_name(cWord))
     end)
 end)
